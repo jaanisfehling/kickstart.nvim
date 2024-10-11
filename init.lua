f6edf44ea90bc933cfa0cd1f3276ec585cbc78f8
Jaanis Fehling <jaanisfehling@gmail.com>
2024-10-12 00:44:06 +0200

    add shortcut for livegrep

--- init.lua
+++ init.lua
@@ -965,6 +965,7 @@ vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-F>', builtin.live_grep, { desc = 'Telescope live grep' })
 
 require('lspconfig').hls.setup {}
 
