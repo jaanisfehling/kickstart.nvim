800303d23417bca6949465cdcb721db81bbc7ec7
Jaanis Fehling <jaanisfehling@gmail.com>
2024-11-13 19:15:44 +0100

    Add python and C/C++ language server, add python formatting

--- init.lua
+++ init.lua
@@ -518,9 +518,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -613,8 +613,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         haskell = { 'fourmolu' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
