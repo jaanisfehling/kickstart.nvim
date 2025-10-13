e9784b4f9c2200c20a5f93c71746ab172604779c
Jaanis Fehling <jaanisfehling@gmail.com>
2025-10-13 14:21:07 +0200

    uncomment js formatter

--- init.lua
+++ init.lua
@@ -615,9 +615,8 @@ require('lazy').setup({
         lua = { 'stylua' },
         haskell = { 'fourmolu' },
         python = { 'isort', 'black' },
-        --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
